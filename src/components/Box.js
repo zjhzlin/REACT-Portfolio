401647860746442dcf36974cce7b9d95d942457e
zjhzlin <smilynnzhang@gmail.com>
2023-03-18 14:40:19 +0000

    first commit

---
+++ src/components/Box.js
@@ -0,0 +1,20 @@
+const Box = ({imageSrc, title, para}) => {
+    return (
+        <div>
+        <div className="text-center shadow-lg p-10 rounded-xl my-10 mx-auto">
+          <img src={imageSrc} alt="girl" width={100} height={100} />
+          <h3 className="text-lg font-medium pt-8 pb-2">
+            {title}
+          </h3>
+          <p className="py-2">
+            {para}
+          </p>
+          <h4 className="py-4 text-teal-600">Tech stack</h4>
+          <p className="text-gray-800 py-1">Java </p>
+          <p className="text-gray-800 py-1">Python </p>
+        </div>
+      </div>
+    )
+}
+
+export default Box;
