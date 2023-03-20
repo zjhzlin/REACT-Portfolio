c88db6dbe2ee5242075ef94112c40d4f20c3b54e
zjhzlin <74384600+zjhzlin@users.noreply.github.com>
2023-03-20 21:43:09 +0000

    delete warning

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,4 @@
 import "./Navbar.css";
-import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
--- src/pages/movies/MovieDetail.js
+++ src/pages/movies/MovieDetail.js
@@ -25,7 +25,7 @@ export const MovieDetail = () => {
 
     useEffect(() => {
         getMovie()
-    }, [id])
+    }, [])
 
     if (!movie.title) return null
 
